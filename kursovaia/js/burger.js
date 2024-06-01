f4faaab78a930de233bf777c232ad71304849663
kapibarka2 <denchikeplay@gmail.com>
2024-06-01 22:14:22 +0300

    upload commit

---
+++ kursovaia/js/burger.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('.burger_menu').click(function(){
+        $('.burger_menu').toggleClass('open-menu');
+        $('.header_btn').toggleClass('open-menu');
+	});
+});
